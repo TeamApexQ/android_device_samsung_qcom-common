c3478f0522f94a090f6fcd96927e613285a01e2c
lbdroid <aserbinski@gmail.com>
2013-09-03 10:46:36 -0400

    mpdecision blob is obsoleted by in-kernel mpdecision

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -213,26 +213,26 @@ case "$target" in
          echo 20000 > /sys/module/msm_dcvs/cores/gpu0/slack_time_min_us
          echo 0 > /sys/module/msm_dcvs/cores/gpu0/slack_mode_dynamic
          # set msm_mpdecision parameters
-         echo 45000 > /sys/module/msm_mpdecision/slack_time_max_us
-         echo 15000 > /sys/module/msm_mpdecision/slack_time_min_us
-         echo 100000 > /sys/module/msm_mpdecision/em_win_size_min_us
-         echo 1000000 > /sys/module/msm_mpdecision/em_win_size_max_us
-         echo 3 > /sys/module/msm_mpdecision/online_util_pct_min
-         echo 25 > /sys/module/msm_mpdecision/online_util_pct_max
-         echo 97 > /sys/module/msm_mpdecision/em_max_util_pct
-         echo 2 > /sys/module/msm_mpdecision/rq_avg_poll_ms
-         echo 10 > /sys/module/msm_mpdecision/mp_em_rounding_point_min
-         echo 85 > /sys/module/msm_mpdecision/mp_em_rounding_point_max
-         echo 50 > /sys/module/msm_mpdecision/iowait_threshold_pct
+#         echo 45000 > /sys/module/msm_mpdecision/slack_time_max_us
+#         echo 15000 > /sys/module/msm_mpdecision/slack_time_min_us
+#         echo 100000 > /sys/module/msm_mpdecision/em_win_size_min_us
+#         echo 1000000 > /sys/module/msm_mpdecision/em_win_size_max_us
+#         echo 3 > /sys/module/msm_mpdecision/online_util_pct_min
+#         echo 25 > /sys/module/msm_mpdecision/online_util_pct_max
+#         echo 97 > /sys/module/msm_mpdecision/em_max_util_pct
+#         echo 2 > /sys/module/msm_mpdecision/rq_avg_poll_ms
+#         echo 10 > /sys/module/msm_mpdecision/mp_em_rounding_point_min
+#         echo 85 > /sys/module/msm_mpdecision/mp_em_rounding_point_max
+#         echo 50 > /sys/module/msm_mpdecision/iowait_threshold_pct
          #set permissions for the nodes needed by display on/off hook
          chown system /sys/module/msm_dcvs/cores/cpu0/slack_time_max_us
          chown system /sys/module/msm_dcvs/cores/cpu0/slack_time_min_us
-         chown system /sys/module/msm_mpdecision/slack_time_max_us
-         chown system /sys/module/msm_mpdecision/slack_time_min_us
+#         chown system /sys/module/msm_mpdecision/slack_time_max_us
+#         chown system /sys/module/msm_mpdecision/slack_time_min_us
          chmod 664 /sys/module/msm_dcvs/cores/cpu0/slack_time_max_us
          chmod 664 /sys/module/msm_dcvs/cores/cpu0/slack_time_min_us
-         chmod 664 /sys/module/msm_mpdecision/slack_time_max_us
-         chmod 664 /sys/module/msm_mpdecision/slack_time_min_us
+#         chmod 664 /sys/module/msm_mpdecision/slack_time_max_us
+#         chmod 664 /sys/module/msm_mpdecision/slack_time_min_us
          soc_id=`cat /sys/devices/system/soc/soc0/id`
          case "$soc_id" in
              "130")
@@ -360,19 +360,19 @@ case "$target" in
 esac
 
 # Post-setup services
-case "$target" in
-    "msm8660" | "msm8960" | "msm8974")
-        start mpdecision
-    ;;
-    "msm7627a")
-        soc_id=`cat /sys/devices/system/soc/soc0/id`
-        case "$soc_id" in
-            "127" | "128" | "129")
-                start mpdecision
-        ;;
-        esac
-    ;;
-esac
+#case "$target" in
+#    "msm8660" | "msm8960" | "msm8974")
+#        start mpdecision
+#    ;;
+#    "msm7627a")
+#        soc_id=`cat /sys/devices/system/soc/soc0/id`
+#        case "$soc_id" in
+#            "127" | "128" | "129")
+#                start mpdecision
+#        ;;
+#        esac
+#    ;;
+#esac
 
 # Enable Power modes and set the CPU Freq Sampling rates
 case "$target" in
